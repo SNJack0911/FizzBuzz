5947c10517df563dd0462618e90dc8cf9dcf1602
Hiraichi <114724153+SNJack0911@users.noreply.github.com>
2023-11-14 10:30:11 +0100

    initial commit

---
+++ test/fizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package fizzBuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFizzBuzz() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
